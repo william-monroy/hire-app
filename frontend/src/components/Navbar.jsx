e51eaf93f23b6866b136f767ba96a2b2a7829e42
Fuentes2395 <afuentesalanis2002@gmail.com>
2022-03-30 10:05:45 -0600

    Homepage & Navbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Navbar.css";
+import {
+  Container,
+  Offcanvas,
+  Nav,
+  NavDropdown,
+  Form,
+  FormControl,
+  Button,
+} from "react-bootstrap";
+import Dropdown from 'react-bootstrap/Dropdown'
+
+const Navbar = () => {
+  return (
+    <div className="Navbar">
+      <img src="assets/denso-logo.png" alt="Logo Denso Navbar" height={25} />
+      <div className="Submenu-usuario">
+        {/* <img src="assets/denso-logo.png" alt="Foto Usuario" height={25}></img> */}
+        {/* Revisar porque NO JALA LO DE ABAJO SASS*/}
+        <Dropdown>
+        <Dropdown.Toggle variant="danger" id="dropdown-basic">
+            "Nombre"
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+            <Dropdown.Item href="#/action-1">Configuración</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Cerrar Sesión</Dropdown.Item>
+        </Dropdown.Menu>
+        </Dropdown>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './App.scss';
 
 // Custom CSS libraries
 import 'bootstrap/dist/css/bootstrap.min.css';
