77e0ed9b32eee1413bf92d11b17546a7c2432fbf
William Monroy <william_monroy777@hotmail.com>
2022-03-29 22:13:54 -0500

    custom theme and libraries

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
+import { Button } from "@nextui-org/react";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
+      <Button color="primary" >Holi</Button>
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+
+// Custom CSS libraries
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { createTheme, NextUIProvider } from '@nextui-org/react';
+
+import './index.css';
+
+const densoLightTheme = createTheme({
+  type: "light",
+  theme: {
+    colors: {
+      // brand colors
+      primaryLight: '#DC0032',
+      primary: '#DC0032',
+      primaryDark: '#DC0032',
+
+      gradient: 'linear-gradient(112deg, $blue100 -25%, $pink500 -10%, $purple500 80%)',
+      link: '#5E1DAD',
+
+      // you can also create your own color
+      myColor: '#ff4ecd'
+
+      // ...  more colors
+    },
+    space: {},
+    fonts: {}
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <NextUIProvider theme={densoLightTheme}>
+      <App />
+    </NextUIProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
